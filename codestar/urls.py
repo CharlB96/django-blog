0bf62ddeaf5ecdcd88adf50608a5c0fe70a37b3f
Charles Bromley <charles.bromley@outlook.com>
2023-11-15 11:41:24 +0000

    Added post_list.html as home page

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.run_blog, name='run_blog')
+    path("", include("blog.urls"), name="blog-urls"),
 ]

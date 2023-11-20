6dc780baa12977742278c24cbf0a421e9e0f7bee
Charles Bromley <charles.bromley@outlook.com>
2023-11-20 09:48:17 +0000

    Added comment adding plus success message

--- blog/models.py
+++ blog/models.py
@@ -23,10 +23,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="post_comment"
+        Post, on_delete=models.CASCADE, related_name="comments"
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comment_author"
+        User, on_delete=models.CASCADE, related_name="commenter"
     )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)

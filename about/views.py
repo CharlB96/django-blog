15288f37fccff39a01859b2373cfae8dbf855713
Charles Bromley <charles.bromley@outlook.com>
2023-11-20 15:14:46 +0000

    Added edit and delete buttons for comments

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
@@ -10,15 +12,23 @@ class AboutList(generic.ListView):
     template_name = "about/about.html"
 
 def about_me(request):
-    """
-    Display an individual :model:`blog.Post`.
-
-    """
-
     about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate = collaborate_form.save(commit=False)
+            collaborate.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )

7b6343ede9c3da036fc3de88f5180e8de905fb1f
Charles Bromley <charles.bromley@outlook.com>
2023-11-21 10:04:22 +0000

    Added cloudinary functionality and image upload

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-charlb96-djangoblog-slgb5o2j71w.ws-eu106.gitpod.io', '.herokuapp.com']
 
